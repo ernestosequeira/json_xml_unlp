4af210f87c4418ff61faafc35135c30eb630d300
Ernesto Sequeira <erdaseq@gmail.com>
2013-07-09 19:36:58 -0300

    UNLP

---
+++ persona.js
@@ -0,0 +1,31 @@
+{
+	"persona": [
+		{ 
+		 	"nombre": "Ernesto", 
+  			"apellido": "Sequeira", 
+  		  	"edad": "28",
+		  	"tipodoc":[
+		 		{ 
+					"DNI": true, 
+		          	"LC": false,
+			  		"LE": false
+			  	},
+			],
+			"nrodoc": "31300000"	
+		},
+		{ 
+		 	"nombre": "Juan", 
+  			"apellido": "Pereira", 
+  		  	"edad": "22",
+		  	"tipodoc":[
+		 		{ 
+					"DNI": true, 
+		          	"LC": false,
+			  		"LE": false
+			  	},
+			],
+			"nrodoc": "31300001"	
+		},
+		
+	]
+}
